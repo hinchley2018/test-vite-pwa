3d691126388d32c18ff064ba41ea9d20c4f42d4b
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2025-02-10 20:32:53 -0600

    min work?

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+const manifest = {
+  registerType: "prompt",
+  manifest: {
+    name: "Test PWA",
+    short_name: "Test PWA",
+    description: "An app",
+    icons: [
+    ],
+    display: "standalone",
+    scope: "/",
+    start_url: "/"
+  }
+}
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),VitePWA(manifest)],
 })
